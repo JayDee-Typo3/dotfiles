1a2397b9751697a1cc9c996269bafb0bacf30e3c
Delesky Johannes <delesky@me.com>
2024-12-11 15:03:03 +0100

    [FEAT]: add wezterm configuration

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,83 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local act = wezterm.action
+local mux = wezterm.mus
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Font configurations.
+config.font_size = 14
+config.line_height = 1.2
+config.font = wezterm.font_with_fallback({
+	{ family = "JetBrainsMono Nerd Font Mono", weight = "Medium", harfbuzz_features = { "calt=0", "clig=0", "liga=0" } },
+	{ family = "JetBrains Mono", weight = "Medium", harfbuzz_features = { "calt=0", "clig=0", "liga=0" } },
+})
+
+config.window_close_confirmation = "NeverPrompt"
+config.skip_close_confirmation_for_processes_named = {
+	"bash",
+	"sh",
+	"zsh",
+	"fish",
+	"tmux",
+	"nu",
+}
+
+config.adjust_window_size_when_changing_font_size = false
+-- For example, changing the color scheme:
+config.color_scheme = "Tokyo Night"
+config.window_background_opacity = 0.8
+
+-- tab bar config
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+
+-- startup
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
+
+-- Keymaps
+config.keys = {
+	{
+		key = ",",
+		mods = "CMD",
+		action = act.SpawnCommandInNewTab({
+			cwd = os.getenv("WEZTERM_CONFIG_DIR"),
+			set_environment_variables = {
+				TERM = "screen-256color",
+			},
+			args = {
+				"/usr/local/bin/nvim",
+				os.getenv("WEZTERM_CONFIG_FILE"),
+			},
+		}),
+	},
+	{
+		key = "R",
+		mods = "CMD|SHIFT",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, _, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+	{
+		key = "t",
+		mods = "CMD|SHIFT",
+		action = act.ShowTabNavigator,
+	},
+	-- other keys
+}
+
+-- and finally, return the configuration to wezterm
+return config
