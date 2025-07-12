589a37bfe386e393ca1d3f78c0f25613bcc5299f
Delesky Johannes <delesky@me.com>
2025-07-12 13:40:15 +0200

    [FEAT]: move nvim into config

--- nvim/lua/config/keymaps.lua
+++
@@ -1,9 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-local opts = { noremap = true, silent = true }
-
-local keymap = vim.keymap.set
-
--- insert mode
-keymap("i", "jk", "<ESC>", opts)
